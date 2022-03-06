e363c2db91e01a9b6648187b73fe4ddaa0421991
dixon <lytone@yeah.net>
2022-03-06 12:23:19 +0800

    edit

--- model/order.js
+++ model/order.js
@@ -63,8 +63,8 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(255),
       allowNull: true
     },
-    uid: {
-      type: DataTypes.INTEGER,
+    openid: {
+      type: DataTypes.STRING(255),
       allowNull: true
     }
   }, {
