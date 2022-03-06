315410df27279cdb27d8ab03a0824c5d4867ac22
dixon <lytone@yeah.net>
2022-03-06 11:40:56 +0800

    edit

--- model/order.js
+++ model/order.js
@@ -56,7 +56,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: true
     },
     flightNum: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING(255),
       allowNull: true
     },
     reason: {
