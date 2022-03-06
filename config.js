1cfc841d0f1dd8114f1568acc289e953f0cb781f
dixon <lytone@yeah.net>
2022-03-06 19:43:43 +0800

    new location

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   tenMapKey:'DC6BZ-TJ36W-THKRM-R5WLQ-LNVJ6-JMBV2',
-  db_name:'',
-  db_username:'',
-  db_pwd:'',
-  db_host:'',
+  db_name:'yun',
+  db_username:'root',
+  db_pwd:'Qfdixon2022',
+  db_host:'10.0.224.5',
   db_port:3306
 }
